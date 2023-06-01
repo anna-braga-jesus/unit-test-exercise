20999df6669de9b098ab2ffb41fce7d20219a788
anna-braga-jesus <annajesus.nave@gmail.com>
2023-06-01 17:25:40 -0300

    fix: result

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -8,22 +8,23 @@ describe("calcule tax unit test suite", () => {
           expect(result).toBe(0);
       });
 
-    it("should return 7,5% if salary is less than 3200 ", ( ) => {
-        const value = generateSalary(2500,3200);
+      it("should return 7.5% if salary is between 2500 and 3200", () => {
+        const value = generateSalary(2500, 3200);
         const result = calculateTax(value);
-        expect(result).toBe(result);
-    });
+        expect(result).toBe(value * 0.075);
+      });
+    
 
       it("should return 15% if salary is less than 4250 ", ( ) => {
           const value = generateSalary(3200,4250);
-          const result = calculateTax(4000);
-          expect(result).toBe(result);
+          const result = calculateTax(value);
+          expect(result).toBe(value * 0.15);
       });
 
       it("should return 27,5% if salary is more than 4250 ", ( ) => {
           const value = generateSalary(4250, 100000);
           const result = calculateTax(value);
-          expect(result).toBe(result);
+          expect(result).toBe(value * 0.275);
       });
 
 })
